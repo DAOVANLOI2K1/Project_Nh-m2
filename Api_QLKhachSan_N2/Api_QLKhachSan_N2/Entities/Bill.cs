d8f171898bc68c1f6286b1edf5ac66151a6719c2
DAOVANLOI2K1 <loinhung4@gmail.com>
2023-02-21 04:46:18 +0700

    update 21-2-2023

---
+++ Api_QLKhachSan_N2/Api_QLKhachSan_N2/Entities/Bill.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_QLKhachSan_N2.Entities
+{
+    public class Bill
+    {
+        /// <summary>
+        /// ID đặt phòng
+        /// </summary>
+        public Guid? IDDP { get; set; }
+
+        /// <summary>
+        /// ID gọi dịch vụ
+        /// </summary>
+        public Guid? IDGoiDV { get; set; }
+
+        /// <summary>
+        /// Tổng tiền phải thanh toán
+        /// </summary>
+        public double? TongTien { get; set; }
+
+        /// <summary>
+        /// Ghi chú hóa đơn
+        /// </summary>
+        public string? GhiChu { get; set; }
+
+        /// <summary>
+        /// ID khách hàng
+        /// </summary>
+        public Guid? KHID { get; set; }
+        public object SoCMTKhachHang { get; set; }
+        public object MaHD { get; set; }
+        public object TrangThai { get; set; }
+    }
+}
